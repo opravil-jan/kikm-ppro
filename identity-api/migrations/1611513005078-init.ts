c0a26dc2867957a8040345e75787e8e23f6433fe
Jaroslav Holemy <whowky@gmail.com>
2021-01-25 20:43:52 +0100

    fix problem with login and .

--- identity-api/migrations/1611513005078-init.ts
+++ identity-api/migrations/1611513005078-init.ts
@@ -11,7 +11,7 @@ export class init1611513005078 implements MigrationInterface {
         await queryRunner.query(`INSERT INTO public.contact (id, "name", "contact", "clientId") VALUES ('63037e43-90c5-4f5b-b29e-702cfb6d176d', 'email', 'jarda@gmail.com', '94e138db-48af-4752-ad40-7efe42a6230c');`)
 //        await queryRunner.query(`​INSERT INTO contact (id, "name", "contact", "clientId") VALUES ('f5281b8d-d669-4a98-8fc7-4526735513e2', 'fax',   '111222333',       '0f136707-d381-464a-8d28-c475ea677593');`)
 //        await queryRunner.query(`​INSERT INTO contact (id, "name", "contact", "clientId") VALUES ('f08db0a3-fcb0-4de2-9f46-5fa00b341690', 'mobil', '765765765',       '0f136707-d381-464a-8d28-c475ea677593');`)
-        await queryRunner.query(`INSERT INTO public.users (id, login, email, password, "createdAt", "deactivationDate") VALUES ('3cca306c-6582-4161-82af-27f8c80b8d77',	'doe.john', 'jarda@gmail.com', '$2a$04$vnlAX9pVAwXbCrXIPeLMyuqcaou1kSpcYlPp8JWhp8N/x1LkkS2Eu', '2021-01-14 06:39:33.977', null);`)
+        await queryRunner.query(`INSERT INTO public.users (id, login, email, password, "createdAt", "deactivationDate") VALUES ('3cca306c-6582-4161-82af-27f8c80b8d77',	'doe', 'jarda@gmail.com', '$2a$04$vnlAX9pVAwXbCrXIPeLMyuqcaou1kSpcYlPp8JWhp8N/x1LkkS2Eu', '2021-01-14 06:39:33.977', null);`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
